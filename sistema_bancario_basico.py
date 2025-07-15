06a4b7d157af3992624ca10ac00654685889643f
Mateusbq <mateusbq510@gmail.com>
2025-07-15 12:24:56 -0300

    Update sistema_bancario_basico.py

--- sistema_bancario_basico.py
+++ sistema_bancario_basico.py
@@ -1,3 +1,4 @@
+# Criação das variáveis nescessárias
 menu = """
 ---------------BANCO BENÍCIO--------------
 
@@ -7,7 +8,6 @@ menu = """
 [q] Sair
 
 => """
-
 saldo = 0
 limite = 500
 extrato = ""
@@ -15,53 +15,69 @@ numero_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
-
+    # Faz com que o menu apareça
     opcao = input(menu)
+    # Faz com que a pessoa possa digitar a letra maiúscula a com espaços  
     opcao = opcao.lower().strip()
 
+    # Configuração de depósito
     if opcao == "d":
         print("\n-> ÁREA DE DEPÓSITO\n")
         valor = float(input("Qual o valor que será depositado?\nR$"))
+        # Caso a pessoa digite um valor inválido a mensegem abaixo será exibida
         if valor <= 0:
             print("\n[ERRO] Por favor, digite um valor a partir de R$1.00")
         else:
             saldo = saldo + valor
-            extrato += f"+ R${valor}"
+            #Faz com que todo valor gigitado entre no extrato
+            extrato += f"+ R${valor}\n"
 
-
+    # Configuração de saque
     elif opcao == "s":
         print("\n-> ÁREA DE SAQUES\n")
+
+        # Delimita a quantidade de saques 
         if LIMITE_SAQUES <= 0:
-             print("\n[ERRO] Limite de saques atingido")
+            print("\n[ERRO] Limite de saques atingido")
+
         else:
             valor = float(input("Qual o valor a ser sacado?\nR$"))
+            # Faz a contagem da quantidade de saques
             LIMITE_SAQUES = LIMITE_SAQUES - 1
-        
+
+        # Delimita o valor do saque
         if valor > 500:
              print("Por favor digite um valor até R$500.00")
 
+        # Faz com que só possa ser sacado o que se tem no saldo
         elif valor > saldo:
              print("\nSALDO INSUFICIENTE.")    
-
+        
         else:
-             saldo = saldo - valor
-             extrato += f"- R${valor}"
-
+            saldo = saldo - valor
+            # Faz com que seja adcionado ao extrato  
+            extrato += f"- R${valor}\n"
+   
+    # Configuração do extrato
     elif opcao == "e":
         print("\n-> EXTRATO")
+        # Caso não haja nada no extrato a mensagem abaixo será exibida
         if extrato == "":
             print("\nNão foram realizadas movimentações\n")
 
         else:
             print(f"\n {extrato}" )
-            print(f"\nVocê tem R${saldo} disponiveis na sua conta.")
+            print(f"Você tem R${saldo} disponiveis na sua conta.")
         
-    
+    # Caso a letra Q for digitada a operação se encerrará
     elif opcao == "q":
         break
 
+    # Se for digitado algo diferente de D, S, E OU Q a mensagem abaixo será exibida
     else:
         print("[ERRO] Opreração inválida, por favor selecione a operação desejada novamente.")
+
+# No final da operação o texto abaixo será exibido
 print("""------------------------------------------
         Obrigado pela preferência! 
                 Até logo!
